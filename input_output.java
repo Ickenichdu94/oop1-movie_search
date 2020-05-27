f85bb0639b014e082d7909990b6bab41d554815a
danielJin00 <danielwodke25@gmail.com>
2020-05-27 03:01:56 +0200

    Update input_output.java

--- input_output.java
+++ input_output.java
@@ -1,3 +1,31 @@
-public class input_output {
-    //Hier kommt der Code von Daniel rein
-}
+import java.io.*;
+
+public interface MoviesearchInterface {
+	/**Fügt den Namen und die Position der Datei , dem Index zu
+	 * @param word 
+	 * @param position
+	 */
+     }
+	
+public class Moviesearch implements MoviesearchInterface{
+		
+		//1.Aufforderung und Verarbeitung der Eingabe des Benutzers
+		
+		System.out.println(" Bitte geben Sie den Namen der Datei ein,die Sie suchen möchten:");
+		Scanner scan = new Scanner(System.in);
+		
+		//2.Analysieren der Datenbank - Zielgerichtet die Filme nach der Eingabe des Benutzers durchgehen
+		br = new BufferedReader(new FileReader(scan.nextLine()));
+	
+		//3.So lange wiederholen bis es keine Zeilen mehr zu lesen gibt
+		while(br.ready()){
+			String line = br.readLine();
+		}
+		//3.Gefundene Datei als String ausgeben
+		
+	
+        }
+	public static void main(String[] args) {
+
+	
+	    }
