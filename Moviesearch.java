87c91f6dff0e7aa2860077c252b1085d25058007
danielJin00 <danielwodke25@gmail.com>
2020-06-03 11:29:09 +0200

    Interface erste Version

---
+++ Moviesearch.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class MovieSearch {
+
+//1. Überschrift Movie Search & Begrüßung
+	public void printHeader() {
+		System.out.println("+--------------------------------------+");
+		System.out.println("|            Willkommen zu             |");
+		System.out.println("|            Movie Search!             |");
+		System.out.println("+--------------------------------------+");
+		}
+//2. Vorstellung der App
+	public void printIntro() {
+		System.out.println("Mit diesem Programm können sie Filme aus der Datenbank suchen");
+	}
+//3. Benutzer nach Input fragen Regisseur oder Filmtitel
+	public void printInput() {
+		System.out.println("Welchen Film möchten Sie suchen?");
+		System.out.println("Bitte geben sie den Titel ein: ");
+	}
+//4. Input scannen vom User
+	
+}
